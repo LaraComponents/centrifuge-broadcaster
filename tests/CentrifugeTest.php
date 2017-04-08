f751ecfe4195110fb197038abc1eec5a5c80b251
Стенин Никита <stenin.nikita@gmail.com>
2017-04-08 17:07:59 +0500

    Add tests

---
+++ tests/CentrifugeTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace LaraComponents\Centrifuge\Test;
+
+class CentrifugeTest extends TestCase
+{
+    public function testGenerateToken()
+    {
+        $timestamp = 1491650279;
+        $user_id = 1;
+        $info = json_encode([
+            'first_name' => 'Nikita',
+            'last_name' => 'Stenin'
+        ]);
+        $client = '0c951315-be0e-4516-b99e-05e60b0cc307';
+        $channel = 'test-channel';
+
+        $clientToken1 = $this->centrifuge->generateToken($user_id, $timestamp);
+        $this->assertEquals('558880399d21bd215e4d1558dd95efad9b82f829a1d44910fb611eeeffac3c50', $clientToken1);
+
+        $clientToken2 = $this->centrifuge->generateToken($user_id, $timestamp, $info);
+        $this->assertEquals('37722e22cee00160d777fd8d594dd831a9d5404016d5515a2cffc7c102ea67ee', $clientToken2);
+
+        $channelSign = $this->centrifuge->generateToken($client, $channel);
+        $this->assertEquals('02fc39b64c252108e80cfdcab2ef774f13a181f5149d21cebabd6eca08d231d2', $channelSign);
+    }
+
+    public function testGenerateApiSign()
+    {
+        $json = json_encode(['method' => 'publish', 'params' => [
+            'channel' => 'test-channel',
+        ]]);
+
+        $apiSign = $this->centrifuge->generateApiSign($json);
+        $this->assertEquals('11950468714031c2e0b95e284482ba6e8d9e17e1a6115eb0db6feded7581ebba', $apiSign);
+    }
+
+    public function testCentrifugeApi()
+    {
+        $publish = $this->centrifuge->publish('test-channel', ['event' => 'test-event']);
+        $this->assertEquals($publish, [
+            'method' => 'publish',
+            'error' => null,
+            'body' => null,
+        ]);
+
+        $broadcast = $this->centrifuge->broadcast(['test-channel-1', 'test-channel-2'], ['event' => 'test-event']);
+        $this->assertEquals($broadcast, [
+            'method' => 'broadcast',
+            'error' => null,
+            'body' => null,
+        ]);
+
+        $presence = $this->centrifuge->presence('test-channel');
+        $this->assertEquals($presence, [
+            'method' => 'presence',
+            'error' => null,
+            'body' => [
+                'channel' => 'test-channel',
+                'data' => [],
+            ],
+        ]);
+
+        $history = $this->centrifuge->history('test-channel');
+        $this->assertEquals($history['method'], 'history');
+        $this->assertEquals($history['error'], null);
+        $this->assertEquals($history['body']['channel'], 'test-channel');
+
+        $channels = $this->centrifuge->channels();
+        $this->assertEquals($channels, [
+            "method" => "channels",
+            "error" => null,
+            "body" => [
+                "data" => [],
+            ],
+        ]);
+
+        $unsubscribe = $this->centrifuge->unsubscribe(1);
+        $this->assertEquals($unsubscribe, [
+            'method' => 'unsubscribe',
+            'error' => null,
+            'body' => null,
+        ]);
+
+        $disconnect = $this->centrifuge->disconnect(1);
+        $this->assertEquals($disconnect, [
+            'method' => 'disconnect',
+            'error' => null,
+            'body' => null,
+        ]);
+
+        $stats = $this->centrifuge->stats();
+        $this->assertEquals($stats['method'], 'stats');
+        $this->assertEquals($stats['error'], null);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LaraComponents\Centrifuge\Test;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use LaraComponents\Centrifuge\CentrifugeServiceProvider;
+
+class TestCase extends Orchestra
+{
+    /**
+     * @var \LaraComponents\Centrifuge\Centrifuge
+     */
+    protected $centrifuge;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->centrifuge = $this->app->make('centrifuge');
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            CentrifugeServiceProvider::class
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('broadcasting.default', 'centrifuge');
+        $app['config']->set('broadcasting.connections.centrifuge', [
+            'driver' => 'centrifuge',
+            'secret' => 'f95bf295-bee6-4259-8912-0a58f4ecd30e',
+            'url' => 'http://localhost:8000',
+            'redis_api' => false,
+            'redis_connection' => 'default',
+        ]);
+    }
+}
